5860d17cacf84dbf7841684f46c9968b6da349cb
jihanchoi <82880933+jihanchoi@users.noreply.github.com>
2024-12-31 10:45:46 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    int ab = stoi(to_string(a) + to_string(b));
+    int ba = stoi(to_string(b) + to_string(a));
+    
+    if(ab >= ba){
+        answer = ab;
+    }else{
+        answer = ba;
+    }
+    return answer;
+}
