ac31df61cd05ff5f20c8face302a6864adcf9b20
jihanchoi <82880933+jihanchoi@users.noreply.github.com>
2025-05-02 09:57:56 +0900

    [level 2] Title: 소수 찾기, Time: 0.61 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <unordered_set>
+using namespace std;
+
+bool isPrime(int a){
+    if(a <= 1) return false;
+    for(int i = 2; i <= sqrt(a); i++){
+        if(a%i == 0) return false;
+    }
+    return true;
+}
+
+int solution(string numbers) {
+    int num = 0;
+    unordered_set<int> answer;
+    
+    sort(numbers.begin(), numbers.end());
+        
+    do{
+        for(int i = 1; i < numbers.size()+1; i++){
+            num = stoi(numbers.substr(0, i));
+            if(isPrime(num)) answer.insert(num);
+        }
+    }while(next_permutation(numbers.begin(), numbers.end()));
+
+    return answer.size();
+}
+
+
+// 소수를 판별
+// 모든 조합을 검사
